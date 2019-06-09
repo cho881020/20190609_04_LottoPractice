8f4220d7160fd26b6019b82359167f88e8976e9f
Kyung Jin Cho <kj_cho@idealidea.co.kr>
2019-06-09 14:50:46 +0900

    사용자에게서 로또 번호 6개 입력받기
    - 1~45의 범위만 허용
    - 중복된 숫자는 불허

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -13,8 +13,66 @@ public class MainDrive {
 		int[] userInputLottoNum = new int[6];
 		
 		Scanner scan = new Scanner(System.in);
+
+		for (int i=0 ; i < userInputLottoNum.length ; i++) {
+			
+			while (true) {
+
+				String messsage =  String.format("%d번째 숫자 입력 : ", i+1);
+				System.out.print(messsage);
+				int tempInput = scan.nextInt();
+				
+				boolean isRangeOk = false;
+				
+				if (1 <= tempInput && tempInput <= 45) {
+					isRangeOk = true;
+				}
+				
+				boolean isDuplOk = true;
+				
+				for (int j=0 ; j < userInputLottoNum.length ; j++) {
+					if (userInputLottoNum[j] == tempInput) {
+						isDuplOk = false;
+					}
+				}
+				
+				if ( isRangeOk && isDuplOk) {
+					userInputLottoNum[i] = tempInput;
+					break;
+				}
+				else if (!isRangeOk) {
+					System.out.println("잘못된 범위의 숫자입니다.");
+					System.out.println("다시 입력해주세요.");
+				}
+				else if (!isDuplOk) {
+					System.out.println("이미 중복된 숫자가 있습니다.");
+					System.out.println("다시 입력해주세요.");
+				}
+				
+				
+			}
+
+		}
+		
+		
 		
 		
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
