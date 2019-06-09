0a7728c1e7ea0ab1c4a02435c850a4214d0ab623
Kyung Jin Cho <kj_cho@idealidea.co.kr>
2019-06-09 14:18:32 +0900

    로또 번호 배열, 스캐너 생성

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,10 +1,19 @@
 package code;
 
+import java.util.Scanner;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
 		
 		System.out.println("로또 프로그램 입니다.");
+		
+		int[] thisWeekLottoNum = {10, 34, 38, 40, 42, 43};
+		
+		int[] userInputLottoNum = new int[6];
+		
+		Scanner scan = new Scanner(System.in);
+		
 		
 	}
 	
